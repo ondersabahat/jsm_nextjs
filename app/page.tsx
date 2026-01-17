0d4ffc3f1e16730ceb9f6a32a75f86cdc33f44f5
Önder <ondersabahat@gmail.com>
2026-01-17 17:30:51 +0300

    finish theme and add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="">
+      <h1 className="h1-bold">Welcome to Nextjs</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Space</h1>
+    </div>
+  );
 };
 
 export default Home;
