6b208fd3bd5fe40a17844a35376075a2b4d59951
Önder <ondersabahat@gmail.com>
2026-01-20 13:50:18 +0300

    implement editor and ask a question feature

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,84 @@
-import { auth } from "@/auth";
+import QuestionCard from "@/components/cards/QuestionCard";
+import HomeFilter from "@/components/filters/HomeFilter";
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
-const Home = async () => {
-  const session = await auth();
-  console.log(session);
-  return <div className="min-h-screen w-full"></div>;
+const questions = [
+  {
+    _id: "1",
+    title: "How to learn React?",
+    description: "I want to learn React, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+      image:
+        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+    },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date("2021-09-01"),
+  },
+  {
+    _id: "2",
+    title: "How to learn JavaScript?",
+    description: "I want to learn JavaScript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "JavaScript" },
+      { _id: "2", name: "React" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+      image:
+        "https://static.vecteezy.com/system/resources/previews/002/002/403/non_2x/man-with-beard-avatar-character-isolated-icon-free-vector.jpg",
+    },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date("2025-10-01"),
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Home = async ({ searchParams }: SearchParams) => {
+  const { query = "", filter = "" } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title.toLowerCase().includes(query.toLowerCase());
+    const matchesFilter = filter ? question.tags[0].name.toLowerCase() === filter.toLowerCase() : true;
+
+    return matchesQuery && matchesFilter;
+  });
+
+  return (
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+        <Button asChild className="primary-gradient text-light-900! min-h-11.5 px-4 py-3">
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch imgSrc="/icons/search.svg" placeholder="Search questions..." route="/" otherClasses="flex-1" />
+      </section>
+      <HomeFilter />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <QuestionCard key={question._id} question={question} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,30 +1,59 @@
-import ROUTES from "@/constants/routes";
 import Link from "next/link";
-import { Badge } from "../ui/badge";
-import { name } from "assert";
+import React from "react";
+
+import ROUTES from "@/constants/routes";
 import { getDeviconClassName } from "@/lib/utils";
 
-interface TagCardProps {
+import { Badge } from "../ui/badge";
+import Image from "next/image";
+
+interface Props {
   _id: string;
   name: string;
-  questions: number;
+  questions?: number;
   showCount?: boolean;
   compact?: boolean;
+  remove?: boolean;
+  isButton?: boolean;
+  handleRemove?: () => void;
 }
 
-const TagCard = ({ _id, name, questions, showCount, compact }: TagCardProps) => {
+const TagCard = ({ _id, name, questions, showCount, compact, handleRemove, isButton, remove }: Props) => {
   const iconClass = getDeviconClassName(name);
-  return (
-    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
-      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
+
+  const content = (
+    <>
+      <Badge className="subtle-medium background-light800_dark300 text-light400_light500 flex flex-row gap-2 rounded-md border-none px-4 py-2 uppercase">
         <div className="flex-center space-x-2">
           <i className={`${iconClass} text-sm`}></i>
           <span>{name}</span>
         </div>
+        {remove && (
+          <Image
+            src="/icons/close.svg"
+            alt="Close icon"
+            width={12}
+            height={12}
+            className="cursor-pointer object-contain invert-0 dark:invert"
+            onClick={handleRemove}
+          />
+        )}
       </Badge>
       {showCount && <p className="small-medium text-dark500_light700">{questions}</p>}
-    </Link>
+    </>
   );
+
+  if (compact) {
+    return isButton ? (
+      <button className="flex justify-between gap-2" type="button">
+        {content}
+      </button>
+    ) : (
+      <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+        {content}
+      </Link>
+    );
+  }
 };
 
 export default TagCard;
