f4301e109b86f45a97fc8f79776b76a09fce1c47
Önder <ondersabahat@gmail.com>
2026-01-20 17:06:17 +0300

    fixed models to newest feature of mongodb types

--- database/account.model.ts
+++ database/account.model.ts
@@ -1,22 +1,23 @@
-import { Schema, model, models, Types } from "mongoose";
+import { Schema, model, models, Types, Document } from "mongoose";
 
 export interface IAccount {
   userId: Types.ObjectId;
   name: string;
   image?: string;
   password?: string;
-  provider: string;
+  provider: string; // e.g., 'github', 'google', 'credentials'
   providerAccountId: string;
 }
 
-const AccountSchema = new Schema(
+export interface IAccountDoc extends IAccount, Document {}
+const AccountSchema = new Schema<IAccount>(
   {
-    userId: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
     name: { type: String, required: true },
     image: { type: String },
     password: { type: String },
     provider: { type: String, required: true },
-    providerAccountId: { type: String, unique: true, required: true },
+    providerAccountId: { type: String, required: true },
   },
   { timestamps: true }
 );
